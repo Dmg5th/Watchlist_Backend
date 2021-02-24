459fabdc3e91909d51898ff76de5f2702a0fa913
Dmg5th <dmgould310@gmail.com>
2021-02-24 13:00:31 -0500

    Added create function for the sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,14 @@
 class SessionsController < ApplicationController
     def create
+      @user = User.find_by(username: params[:session][:username])
+      
+      if @user && @user.authenticate(params[:session][:password])
+
+        render json: @user 
+      else
+        render json: {
+            error: "Invalid credentials"
+        }
     end
 
     
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get "/login", to: "sessions#create"
+  post "/login", to: "sessions#create"
   
   resources :movies
   resources :users
   resources :user_movies
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+ 
