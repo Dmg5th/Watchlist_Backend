199f0a6cf4cb4420b33013f645fde4c1b2cec203
Dmg5th <dmgould310@gmail.com>
2021-02-25 14:34:44 -0500

    AAdded controller actions to the user_movies, if logged in

--- app/controllers/user_movies_controller.rb
+++ app/controllers/user_movies_controller.rb
@@ -1,8 +1,14 @@
 class UserMoviesController < ApplicationController
 
     def index
-        watchlist = UserMovie.all
-
-        render json: watchlist
+        if logged_in?
+            @watchlist = current_user.user_movies
+            
+            render json: current_user.user_movies
+        else 
+            render json: {
+                error: "You must be logged in to see your watchlist"
+            }
+        end 
     end 
 end
