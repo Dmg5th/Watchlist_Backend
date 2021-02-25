785053d377471963c6442c31842acda392e78256
Dmg5th <dmgould310@gmail.com>
2021-02-25 14:49:31 -0500

    Scaffolded the user_movies rest of controller to get other actions

--- app/controllers/user_movies_controller.rb
+++ app/controllers/user_movies_controller.rb
@@ -1,4 +1,6 @@
 class UserMoviesController < ApplicationController
+    before_action :set_user_movie, only: [:show, :update, :destroy]
+
 
     def index
         if logged_in?
@@ -11,4 +13,59 @@ class UserMoviesController < ApplicationController
             }
         end 
     end 
+     # GET /trips/1
+  def show
+    render json: @watchlist
+  end
+
+  # POST /trips
+  def create
+  
+    @watchlist = current_user.user_movies.build(user_movie_params)
+
+    if @watchlist.save
+      render json:  @watchlist, status: :created
+    else
+      error_resp = {
+        error: @watchlist.errors.full_messages.to_sentence
+      }
+      render json: error_resp, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /trips/1
+  def update
+    if @watchlist.update(user_movie_params)
+      render json:  @watchlist, status: :ok
+    else
+      error_resp = {
+        error: @watchlist.errors.full_messages.to_sentence
+      }
+      render json: error_resp, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /user_movie/1
+  def destroy
+    if @watchlist.destroy
+      render json:  { data: "Watchlist successfully destroyed" }, status: :ok
+    else
+      error_resp = {
+        error: "Watchlist not found and not destroyed"
+      }
+      render json: error_resp, status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user_movie
+      @watchlist = UserMovie.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def user_movie_params
+      params.require(:user_movie).permit("*")
+    end
 end
+
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer
   include JSONAPI::Serializer
   attributes :username, :name
+  has_many :trips, serializer: TripSerializer
 end
