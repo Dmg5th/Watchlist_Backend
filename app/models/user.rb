236c3b8ea92f8508ca333ea5db2ea9af55e6404c
Dmg5th <dmgould310@gmail.com>
2021-03-01 10:13:29 -0500

    Made validations for a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-
     has_many :user_movies
     has_many :movies, through: :user_movies
+
+    validates :name, :username, presence: true
  end
