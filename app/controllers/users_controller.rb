b1201626b0ebd7f5234ef052349b14098c58e43f
Dmg5th <dmgould310@gmail.com>
2021-03-01 11:50:10 -0500

    Made more validations for the User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,10 @@ class UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-
+    
     if @user.save
-      render json: @user, status: :created, location: @user
+      session[:user_id] = @user.id
+      render json: UserSerializer.new(@user), status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
     has_many :movies, through: :user_movies
 
     validates :name, :username, presence: true
+    validates :username, uniquness: true 
  end
